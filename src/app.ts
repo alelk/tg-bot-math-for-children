7027808697242e11166ab9d5dfea45d31ad69a89
Alex Elkin <elckinne@gmail.com>
2021-11-17 20:03:56 +0300

    chore(build): configure nodemon.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import { Telegraf } from "telegraf";
 
 const tg = new Telegraf("2140755676:AAFBxSGQdLhpzgki8kFjRYQISBAuECW0OZo")
 
-tg.start((ctx) => ctx.reply("Добро пожаловать!"))
+tg.start((ctx) => ctx.reply("Добро пожаловать!!!"))
 
 tg.launch()
